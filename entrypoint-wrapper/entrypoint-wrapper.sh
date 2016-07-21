6cf5802aa427db0bad62b370ce5fcb1edc31a30a
Ferris <ferris@ferrisellis.com>
2016-07-21 17:38:35 +0000

    Working on entrypoint-wrapper.sh

--- entrypoint-wrapper/entrypoint-wrapper.sh
+++ entrypoint-wrapper/entrypoint-wrapper.sh
@@ -30,18 +30,20 @@ verify_installed() {
   command -v $PROGRAM >/dev/null 2>&1 || { echo >&2 "Require $PROGRAM but it's not installed.  Aborting."; exit 1; }
   echo "Verified that $PROGRAM is installed."
 }
+export -f verify_installed
 
 verify_vars_exist() {
-  for var in $1; do
+  for var in $1 ; do
     if [ -n "${!var}" ]; then
       echo "env var ${var}=${!var}"
     else
-      echo "env var ${var} is not defined but needed for startup script!"
+      echo "ERROR: env var ${var} is not defined but needed for startup script!"
       echo "exiting..."
       exit 1
     fi
   done
 }
+export -f verify_vars_exist
 
 if [ "$DATASTORE_TYPE" != "volume" ]; then
   echo -e "Detected remote datastore type '$DATASTORE_TYPE', starting to retrieve datastore..."
@@ -52,7 +54,7 @@ if [ "$DATASTORE_TYPE" != "volume" ]; then
   case $DATASTORE_TYPE in
     git)
       export NEEDED_VARS="$NEEDED_VARS DATASTORE_GIT_ADDRESS DATASTORE_GIT_BRANCH"
-      verify_vars_exist "$NEEDED_VERS"
+      verify_vars_exist "$NEEDED_VARS"
       verify_installed git
       echo -e "Git cloning git repo:\n\tgit address: $DATASTORE_GIT\n\tbranch: $DATASTORE_GIT_BRANCH\n\tclone dir: $DATASTORE_DIR"
       git clone --branch $DATASTORE_GIT_BRANCH $DATASTORE_GIT_ADDRESS $DATASTORE_DIR
@@ -63,8 +65,8 @@ if [ "$DATASTORE_TYPE" != "volume" ]; then
     ;;
     http-tar)
       export NEEDED_VARS="$NEEDED_VARS DATASTORE_HTTP_ADDRESS"
-      verify_vars_exist "$NEEDED_VERS"
-      verify-installed curl tar
+      verify_vars_exist "$NEEDED_VARS"
+      verify_installed curl tar
       TMP_DATASTORE_PATH="/tmp/DATASTORE_PULL.tar.gz"
       echo -e "curl downlading: $DATASTORE_HTTP_ADDRESS"
       curl -L $DATASTORE_HTTP_ADDRESS --output $TMP_DATASTORE_PATH
@@ -95,7 +97,7 @@ fi
 
 verify_installed dumb-init
 
-if [ "INSIDE_DUMB_INIT" != "true" ]; then
+if [ "$INSIDE_DUMB_INIT" != "true" ]; then
   echo -e "Detected that entrypoint isn't wrapped in dumb-init!!!"
   echo -e "Make sure that entrypoint is: export INSIDE_DUMB_INIT=\"true\"; dumb-init /entrypoint-wrapper.sh"
   exit 1
