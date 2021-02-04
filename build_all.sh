1b80baeb1d0701e7c74706618a8e9cbece0169a9
blag <blag@users.noreply.github.com>
2021-02-04 10:57:33 -0700

    Also handle Dockerfiles within systemd directories

--- build_all.sh
+++ build_all.sh
@@ -17,5 +17,9 @@ done
 for pt in $(find packagingtest -name Dockerfile); do
 	path=$(dirname $pt)
 	flavor=$(basename $path)
+	if [[ "$flavor" == "systemd" ]]; then
+		path=$(dirname $pt)
+		flavor=$(basename $(dirname $path))
+	fi
 	(cd $path; docker build -t stackstorm/packagingtest:$flavor .) || exit -1
 done
