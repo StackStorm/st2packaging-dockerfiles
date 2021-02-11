dad0ac744f007b56722de3259a4eac1b12579ca0
blag <blag@users.noreply.github.com>
2021-02-11 13:16:33 -0700

    Convert indentation to spaces

--- build_all.sh
+++ build_all.sh
@@ -3,13 +3,13 @@
 set -eux
 
 for pb in $(find packagingbuild -name Dockerfile); do
-	path=$(dirname $pb)
-	flavor=$(basename $path)
-	(cd $path; docker build -t stackstorm/packagingbuild:$flavor .) || exit -1
+    path=$(dirname $pb)
+    flavor=$(basename $path)
+    (cd $path; docker build -t stackstorm/packagingbuild:$flavor .) || exit -1
 done
 
 for pt in $(find packagingtest -name Dockerfile); do
-	path=$(dirname $pt)
-	flavor=$(basename $path)
-	(cd $path; docker build -t stackstorm/packagingtest:${flavor}-systemd .) || exit -1
+    path=$(dirname $pt)
+    flavor=$(basename $path)
+    (cd $path; docker build -t stackstorm/packagingtest:${flavor}-systemd .) || exit -1
 done
